68a39f0dc0dc03364f885d68c4ca028fd479617c
pan2dora <pan2dora.brown@gmail.com>
2023-03-09 13:11:37 -0500

    code where the program can differ if the question is multiple choice or t/f

--- W8-trivia-app/client/src/components/questioncard.js
+++ W8-trivia-app/client/src/components/questioncard.js
@@ -1,5 +1,34 @@
 const QuestionCard = (props) => {
-    
+  if(props.question.type === "boolean"){
+    return (
+      <div className={"question-section"}>
+        <div className="question-text">{props.question.question}</div>
+        <div className="answer-section">
+          <div>
+            <button>True</button>
+            <button>False</button>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={"question-section"}>
+        <div className="question-text">{props.question.question}</div>
+        <div className="answer-section">
+          <div>
+            <button>Multiple</button>
+            <button>Choice</button>
+          </div>
+        </div>
+      </div>
+    );
+
+  }
+  }
+ 
+  
+  /*  
     return (
       <div className={"question-section"}>
         <div className='question-text'>{props.question.question}</div>
@@ -10,5 +39,6 @@ const QuestionCard = (props) => {
       </div>
     );
   };
+  */
 
 export default QuestionCard;
