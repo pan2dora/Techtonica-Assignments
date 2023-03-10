e15a01a9b2698aaf9df75b8d13fb521d2bd741fb
pan2dora <pan2dora.brown@gmail.com>
2023-03-10 09:57:06 -0500

    added buttons and changed numbers index

--- W8-trivia-app/client/src/components/questioncard.js
+++ W8-trivia-app/client/src/components/questioncard.js
@@ -1,44 +1,26 @@
 const QuestionCard = (props) => {
-  if(props.question.type === "boolean"){
-    return (
-      <div className={"question-section"}>
-        <div className="question-text">{props.question.question}</div>
-        <div className="answer-section">
-          <div>
-            <button>True</button>
-            <button>False</button>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className={"question-section"}>
-        <div className="question-text">{props.question.question}</div>
-        <div className="answer-section">
-          <div>
-            <button>Multiple</button>
-            <button>Choice</button>
-          </div>
-        </div>
-      </div>
-    );
+  let incorrectAnswersArray = props.question.incorrect_answers;
+  let correctAnswers = props.question.correct_answer;
+  // let answersShuffled = answersArray.Math.floor(Math.random()*answersArray.length)
 
-  }
-  }
- 
-  
-  /*  
-    return (
-      <div className={"question-section"}>
-        <div className='question-text'>{props.question.question}</div>
-        <div className='answer-section'>
-		<button>True</button>
-		<button>False</button>
+  return (
+    <div className={"question-section"}>
+      <div className="question-count">
+        <span>Question {props.question_num}/{props.questions_count}</span>
+      </div>
+      <div className="question-text">{props.question.question}</div>
+
+      <div className="answer-section">
+        <div>
+          <button>{correctAnswers}</button>
+
+          {incorrectAnswersArray.map(function (answers) {
+            return <button>{answers}</button>;
+          })}
         </div>
       </div>
-    );
-  };
-  */
+    </div>
+  );
+};
 
 export default QuestionCard;
