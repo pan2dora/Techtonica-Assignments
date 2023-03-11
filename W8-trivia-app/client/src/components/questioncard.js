15243c6e675e05b3e0c8e436e313d371f46b60f0
pan2dora <pan2dora.brown@gmail.com>
2023-03-10 20:21:48 -0500

    changed functionality

--- W8-trivia-app/client/src/components/questioncard.js
+++ W8-trivia-app/client/src/components/questioncard.js
@@ -1,7 +1,50 @@
+import React, { useState } from 'react'
+
 const QuestionCard = (props) => {
   let incorrectAnswersArray = props.question.incorrect_answers;
   let correctAnswers = props.question.correct_answer;
   // let answersShuffled = answersArray.Math.floor(Math.random()*answersArray.length)
+  const newAnswerArray= incorrectAnswersArray.concat(correctAnswers)
+const [answered,setAnswered] = useState(false)
+
+
+const handleClickMC= (userAnswer) =>{
+  if (userAnswer === props.question.correct_answer){
+    setAnswered(true)
+  }else{
+    setAnswered(true)
+   
+  }
+}
+
+const handleClickTF= (userAnswer) =>{
+  if (userAnswer === props.question.correct_answer){
+    setAnswered(true)
+  }else if(userAnswer === props.question.incorrect_answers){
+    setAnswered(true)
+   
+  }
+}
+
+if (props.question.type ==="boolean"){
+return (
+<div className={"question-section"}>
+      <div className="question-count">
+        <span>Question {props.question_num}/{props.questions_count}</span>
+      </div>
+      <div className="question-text">{props.question.question}</div>
+
+      <div className="answer-section">
+        <div>
+          
+<button disabled = {answered} style ={{backgroundColor: "true" ? "white":"yellow"}} onClick={()=>handleClickTF()  }> True</button>
+<button disabled = {answered} style ={{backgroundColor: "false" ? "white":"yellow"}} onClick={()=>handleClickTF()}>False</button>
+        </div>git 
+      </div>
+    </div>
+)
+
+}else{
 
   return (
     <div className={"question-section"}>
@@ -12,15 +55,15 @@ const QuestionCard = (props) => {
 
       <div className="answer-section">
         <div>
-          <button>{correctAnswers}</button>
+          
 
-          {incorrectAnswersArray.map(function (answers) {
-            return <button>{answers}</button>;
+          {newAnswerArray.map(function (answers) {
+            return <button disabled ={answered} onClick={()=>handleClickMC()}>{answers}</button>;
           })}
         </div>
       </div>
     </div>
   );
-};
+}};
 
 export default QuestionCard;
